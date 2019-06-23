5fc1f5edef043b0c39cfab20b85182fc1bb787f6
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-06-23 11:10:11 +0200

    Added main python file to launch the program

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+
+import wx
+
+import gui
+
+if __name__ == "__main__":
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-b", "--start-in-background", action="store_true",
+                                 help="Start the program in the background with the previous configuration")
+    arguments = argument_parser.parse_args()
+
+    app = wx.App()
+    main_frame = gui.MainFrame()
+    if not arguments.start_in_background:
+        main_frame.Show()
+    app.MainLoop()
