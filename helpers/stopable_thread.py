f65210b2f0b1540ee0ea8f33ac7b2bd76597caf8
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-06-27 19:11:31 +0200

    Created helpers module

---
+++ helpers/stopable_thread.py
@@ -0,0 +1,22 @@
+import threading
+
+
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method.
+
+    The thread itself has to check regularly for the stopped() condition.
+
+    Copied from: https://stackoverflow.com/a/325528
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(StoppableThread, self).__init__(*args, **kwargs)
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        """Signals to the thread that it should stop."""
+        self._stop_event.set()
+
+    def stopped(self) -> bool:
+        """Return whether the thread is supposed to be stopped."""
+        return self._stop_event.is_set()
--- start_menu_helper.py
+++ start_menu_helper.py
@@ -1,11 +1,11 @@
 import getpass
 import pathlib
-import threading
 import time
 from typing import List
 
 import configuration
-import windows_shortcuts
+from helpers import windows_shortcuts
+from helpers.stopable_thread import StoppableThread
 
 
 class StartMenuHelper:
@@ -211,24 +211,3 @@ def resolve_files(files: List[pathlib.WindowsPath]) -> List[pathlib.WindowsPath]
         else:
             resolved_files.append(file.resolve())
     return resolved_files
-
-
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method.
-
-    The thread itself has to check regularly for the stopped() condition.
-
-    Copied from: https://stackoverflow.com/a/325528
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        """Signals to the thread that it should stop."""
-        self._stop_event.set()
-
-    def stopped(self) -> bool:
-        """Return whether the thread is supposed to be stopped."""
-        return self._stop_event.is_set()
