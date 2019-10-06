86d29b06263a4ba75ecad7cae2b7f77578b38814
Jarik Marwede <accounts.jarikmarwede@zoho.eu>
2019-10-06 11:44:59 +0200

    Fixed mistake from previous commit

--- scripts/generate_executable.cmd
+++ scripts/generate_executable.cmd
@@ -1,5 +1,5 @@
 :: Run pyinstaller to produce an executable
 pyinstaller --distpath "%~dp0.." --clean --onefile --specpath "%~dp0..\build" --name "Start Menu Helper" --add-data "../icon.ico;." --noconsole --icon "../icon.ico" --uac-admin "%~dp0..\main.py"
 :: Remove folders containing build information created by pyinstaller
-rmdir build /s /q
+rmdir "%~dp0build" /s /q
 rmdir "%~dp0..\build" /s /q
