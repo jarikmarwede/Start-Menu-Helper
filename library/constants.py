56b0a530b60ed3b990f0b33de45620f27cc62c9d
Jarik Marwede <contact@jarikmarwede.com>
2024-03-13 19:02:59 +0100

    Log path of duplicated files

--- library/constants.py
+++ library/constants.py
@@ -5,20 +5,20 @@ PROGRAM_NAME = "Start Menu Helper"
 VERSION_NUMBER = "1.1.2"
 ICON_FILE_NAME = "icon.ico"
 TIME_BETWEEN_SCANS_IN_MINUTES = 5
-APP_DATA_PATH = pathlib.Path.home().joinpath("AppData")
+APP_DATA_PATH = pathlib.WindowsPath.home().joinpath("AppData")
 CONFIGURATION_PATH = APP_DATA_PATH.joinpath("Roaming").joinpath(PROGRAM_NAME)
 CONFIGURATION_FILE_PATH = CONFIGURATION_PATH.joinpath("config.ini")
 LOG_FILE_PATH = CONFIGURATION_PATH.joinpath("log.txt")
-STARTUP_PATH = pathlib.Path.home().drive + \
+STARTUP_PATH = pathlib.WindowsPath.home().drive + \
                "\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
-EXECUTABLE_PATH = pathlib.Path.cwd().joinpath(PROGRAM_NAME + ".exe")
+EXECUTABLE_PATH = pathlib.WindowsPath.cwd().joinpath(PROGRAM_NAME + ".exe")
 START_MENU_PATHS = [
-    pathlib.WindowsPath(pathlib.Path.home().drive + "/ProgramData/Microsoft/Windows/Start Menu"),
+    pathlib.WindowsPath(pathlib.WindowsPath.home().drive + "/ProgramData/Microsoft/Windows/Start Menu"),
     APP_DATA_PATH.joinpath("Roaming/Microsoft/Windows/Start Menu")
 ]
 START_MENU_PROGRAMS_PATHS = [
     pathlib.WindowsPath(
-        pathlib.Path.home().drive + "/ProgramData/Microsoft/Windows/Start Menu/Programs"),
+        pathlib.WindowsPath.home().drive + "/ProgramData/Microsoft/Windows/Start Menu/Programs"),
     APP_DATA_PATH.joinpath("Roaming/Microsoft/Windows/Start Menu/Programs")
 ]
 PROTECTED_FOLDERS = [
