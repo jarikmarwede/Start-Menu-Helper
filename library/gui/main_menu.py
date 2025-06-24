cc14b6a3ad76eac7eaa79e61a8d890de4ae40ff4
Jarik Marwede <contact@jarikmarwede.com>
2025-06-24 22:30:54 +0200

    Fix linter issues

--- library/gui/main_menu.py
+++ library/gui/main_menu.py
@@ -11,22 +11,26 @@ ID_OPEN_ISSUE_TRACKER = wx.NewId()
 
 class MainMenu(wx.MenuBar):
     """Main menu bar widget."""
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize main menu bar."""
         super().__init__()
 
+        self.add_help_menu()
+
+        self.Bind(wx.EVT_MENU, self.handle_menu_click)
+
+    def add_help_menu(self) -> None:
+        """Append the help menu to the menu bar."""
         help_menu = wx.Menu()
         help_menu.Append(ID_OPEN_DOCUMENTATION, "Documentation", "Open the online documentation")
         help_menu.Append(ID_OPEN_ISSUE_TRACKER, "Report issue", "Open the issue tracker")
 
         self.Append(help_menu, "Help")
 
-        self.Bind(wx.EVT_MENU, self.handle_menu_click)
-
     def handle_menu_click(self, event: wx.MenuEvent) -> None:
         """Handle menu item selection."""
-        id = event.GetId()
-        if id == ID_OPEN_DOCUMENTATION:
+        event_id = event.GetId()
+        if event_id == ID_OPEN_DOCUMENTATION:
             webbrowser.open_new_tab(DOCUMENTATION_URL)
-        elif id == ID_OPEN_ISSUE_TRACKER:
+        elif event_id == ID_OPEN_ISSUE_TRACKER:
             webbrowser.open_new_tab(ISSUE_TRACKER_URL)
